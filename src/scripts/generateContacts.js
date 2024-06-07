c79a22c31351cf292aa7c3aba8c2a1d785081dbf
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-06-07 17:05:54 +0300

    step 1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let currentContacts = JSON.parse(data);
+
+    let newContacts = [];
+
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const allContacts = currentContacts.concat(newContacts);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(allContacts), 'utf-8');
+  } catch (error) {
+    console.error('Помилка додавання(читання) даних:', error);
+  }
+};
 
 await generateContacts(5);
