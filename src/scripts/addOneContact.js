73b348f6d23676a3f8055d426ab5f86c18044c12
Yevgen Vlasenko <evgenvl@ukr.net>
2024-06-08 21:11:28 +0300

    all steps

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -4,9 +4,15 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const addOneContact = async () => {
   try {
-    const currentContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
-    const newContacts = currentContacts.push(createFakeContact());
-    await fs.writeFile(PATH_DB, JSON.stringify(newContacts, null, 2), 'utf-8');
+    let currentContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+
+    currentContacts.push(createFakeContact());
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(currentContacts, null, 2),
+      'utf-8',
+    );
   } catch (error) {
     console.error('Помилка додавання(читання) даних:', error);
   }
--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const currentContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    let newContacts = [];
+    for (const contact of currentContacts) {
+      if (Math.random() >= 0.5) {
+        newContacts.push(contact);
+      }
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(newContacts, null, 2), 'utf-8');
+  } catch (error) {
+    console.error('Помилка додавання(читання) даних:', error);
+  }
+};
 
 await thanos();
